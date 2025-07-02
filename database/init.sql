1e21a424e7d5093f593195f8097e4dc1a46ffd7d
TheHunter896 <david_corp896@outlook.es>
2025-07-02 20:21:52 +0200

    Add sql tables

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,14 @@
-CREATE TABLE status(
+CREATE TABLE status
+(
     process varchar(250) NOT NULL,
-    status boolean NOT NULL DEFAULT FALSE
-)
+    status  boolean      NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE user_histories(
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    username varchar(250) NOT NULL PRIMARY KEY,
+    funny_url varchar(250) NOT NULL,
+    social_url varchar(250) NOT NULL,
+    created_at timestamp,
+    updated_at timestamp DEFAULT NOW()
+);
