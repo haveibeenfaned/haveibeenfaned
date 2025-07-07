eeeb511695bcc8672f71f22ff980235c30018dde
TheHunter896 <david_corp896@outlook.es>
2025-07-07 09:26:14 +0200

    Todos are done

--- main.py
+++ main.py
@@ -3,5 +3,3 @@ import uvicorn
 from api.app import app
 
 # fastapi dev / prod runs this imported app
-# TODO: Fix frontend for different kind of responses from API
-# TODO: Fix API for different kind of responses from Crawler
